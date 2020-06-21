612c0e818540fa8f55db1a7001e8d8a422e5b460
shanemoyo <smoyo@pdx.edu>
2020-06-20 17:43:18 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  const [board, setboard] = useState(["", "", "","", "", "","", "", "",]);
+  const [turn, setTurn] = useState("X") ; 
+  const handleClick = (index) => { 
+    console.log(`givinig square ${index} value of ${turn}`);
+    const newTurn = turn === "X" ? "O" : "X"; 
+    setTurn(newTurn); 
+    const newBoard = board; 
+    newBoard[index] = turn; 
+    setboard(board); 
+  }
+
+  const checkForWin = () => { 
+   
+  }
   return (
     <div className="App">
+      <h1> Tic Tac Toe </h1>
       <div className="board">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div> 
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div> 
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div> 
+        {board.map((square, index) => <div key={index} className="square" onClick={() => handleClick(index)}>{square}</div>)}
       </div> 
     </div>
   );
